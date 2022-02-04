7ec3d4713e1a223e29e90df658073d278931c961
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-02-04 11:35:00 -0400

    feat: add new routes in module

--- lib/app/modules/splash/splash_module.dart
+++ lib/app/modules/splash/splash_module.dart
@@ -2,12 +2,14 @@ import 'package:em_cartaz/app/modules/home/home_page.dart';
 import 'package:em_cartaz/app/modules/splash/splash_Page.dart';
 import 'package:em_cartaz/app/modules/splash/splash_store.dart';
 import 'package:em_cartaz/app/modules/user_identification/userIdentification_page.dart';
+import 'package:em_cartaz/app/modules/user_identification/user_identification_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class SplashModule extends Module {
   @override
   final List<Bind> binds = [
     Bind.lazySingleton((i) => SplashStore()),
+    Bind((i) => UserIdentificationStore()),
   ];
 
   @override
@@ -16,6 +18,11 @@ class SplashModule extends Module {
     ChildRoute(
       '/user/identification',
       child: (_, args) => const UserIdentificationPage(),
+      transition: TransitionType.fadeIn,
+    ),
+    ChildRoute(
+      '/home',
+      child: (_, args) => const HomePage(),
       transition: TransitionType.fadeIn,
     ),
   ];
