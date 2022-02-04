6f27c9e4669020e118b3a51c68d809bc18a33162
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-02-04 10:38:44 -0400

    feat: add global classes for color, images, styles text

---
+++ lib/app/core/themes/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const Color darkGradientFirst = Color(0xFF221f2b);
+  static const Color darkGradientSecond = Color(0xFF5f576e);
+  static const Color red = Color(0xFFe02d42);
+}
---
+++ lib/app/core/themes/app_styles.dart
@@ -0,0 +1,11 @@
+import 'package:em_cartaz/app/core/themes/app_colors.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppStyles {
+  static final heading = GoogleFonts.poppins(
+    color: AppColors.darkGradientSecond,
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+  );
+}
