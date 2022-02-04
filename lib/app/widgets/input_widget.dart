077a0e8ea7bfdf6cb5f1eb10709abf7120d1a5ca
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-02-04 11:04:12 -0400

    feat: add widget button and input

---
+++ lib/app/widgets/input_widget.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:em_cartaz/app/core/themes/app_colors.dart';
+import 'package:em_cartaz/app/core/themes/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class InputWidget extends StatelessWidget {
+  const InputWidget({
+    Key? key,
+    this.title,
+    this.onChange,
+    this.inputAction,
+  }) : super(key: key);
+
+  final String? title;
+  final Function(String? value)? onChange;
+  final TextInputAction? inputAction;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      style: AppStyles.heading,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.darkGradientSecond),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.darkGradientSecond),
+        ),
+        labelText: title,
+        labelStyle: AppStyles.heading,
+      ),
+      cursorColor: AppColors.darkGradientSecond,
+      onChanged: onChange,
+      textInputAction: inputAction ?? TextInputAction.unspecified,
+    );
+  }
+}
