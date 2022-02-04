4df3ffe4df451172f4845783a488b3f1bea98cb6
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-02-04 11:34:32 -0400

    chore: remove import libs not used

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,7 +1,4 @@
-import 'package:em_cartaz/app/modules/home/home_module.dart';
 import 'package:em_cartaz/app/modules/splash/splash_module.dart';
-import 'package:em_cartaz/app/modules/splash/splash_page.dart';
-import 'package:em_cartaz/app/modules/user_identification/userIdentification_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
