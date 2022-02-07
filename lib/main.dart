1f9755217fc4158147d27622be0d85f32aa9c849
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-02-07 08:11:44 -0400

    refactor: configure archive main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'package:em_cartaz/app/core/themes/app_colors.dart';
+import 'package:em_cartaz/app/database/database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() =>
-    runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DatabaseHelper.instance;
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: AppColors.darkGradientFirst,
+    ),
+  );
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    ),
+  );
+}
