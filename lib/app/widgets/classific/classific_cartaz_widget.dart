f14ee4806891a904c673c4861479281bc69000e1
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-02-08 15:09:37 -0400

    feat: add component  for classfic movie

---
+++ lib/app/widgets/classific/classific_cartaz_widget.dart
@@ -0,0 +1,69 @@
+import 'package:em_cartaz/app/core/themes/app_colors.dart';
+import 'package:em_cartaz/app/modules/home/models/cartaz_models.dart';
+import 'package:flutter/material.dart';
+
+class ClassificCartazWidget extends StatelessWidget {
+  const ClassificCartazWidget({Key? key, this.event}) : super(key: key);
+
+  final MovieCartaz? event;
+
+  @override
+  Widget build(BuildContext context) {
+    var colors = Colors.transparent;
+    var title = "";
+
+    switch (event?.contentRating) {
+      case '12 anos':
+        colors = Colors.yellowAccent.shade700;
+        title = '12';
+        break;
+      case '10 anos':
+        colors = Colors.blueAccent.shade700;
+        title = '10';
+        break;
+      case '14 anos':
+        colors = Colors.orangeAccent.shade700;
+        title = '14';
+        break;
+      case '16 anos':
+        colors = Colors.redAccent.shade700;
+        title = '16';
+        break;
+      case '18 anos':
+        colors = Colors.black;
+        title = '18';
+        break;
+      case 'Livre':
+        colors = Colors.greenAccent.shade700;
+        title = 'L';
+        break;
+
+      default:
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 36,
+        width: 36,
+        decoration: BoxDecoration(
+          color: colors,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: AppColors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
