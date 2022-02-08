d78fdb3fb7efd2033ba137bb69bcd12bc85b06a9
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-02-08 16:33:21 -0400

    chore: rename for component file

--- lib/app/widgets/cards/card_soon_widget.dart
+++ lib/app/widgets/cards/card_soon_widget.dart
@@ -1,6 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:em_cartaz/app/modules/home/models/soon_models.dart';
-import 'package:em_cartaz/app/widgets/classific/classific_soon_widget%20copy.dart';
+import 'package:em_cartaz/app/widgets/classific/classific_soon_widget.dart';
 import 'package:flutter/material.dart';
 
 class CardSoonWidget extends StatelessWidget {
