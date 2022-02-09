3927f5f629349e8633b8bc63f0dab266528e558b
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-02-09 15:11:01 -0400

    feat: add get duration in hours

--- lib/app/modules/details_destaques/detailsDestaques_store.dart
+++ lib/app/modules/details_destaques/detailsDestaques_store.dart
@@ -1,4 +1,3 @@
-import 'package:em_cartaz/app/modules/home/models/destaques_models.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 
@@ -8,7 +7,19 @@ class DetailsDestaquesStore = _DetailsDestaquesStoreBase
     with _$DetailsDestaquesStore;
 
 abstract class _DetailsDestaquesStoreBase with Store {
+  @observable
+  String? movieHours;
+
   returnPage() async {
     Modular.to.navigate('/home/');
+  }
+
+  @action
+  getDuration(int? duration) {
+    final hour = duration! / 60;
+
+    final minutes = duration % 60;
+
+    return movieHours = '${hour.floor()} : $minutes';
   }
 }
