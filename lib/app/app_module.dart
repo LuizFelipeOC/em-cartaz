48475b42b242de6e2a22d5ff783e47be44f0b94e
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-02-10 09:03:46 -0400

    feat: add details generic modules in modules routes

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,4 +1,5 @@
 import 'package:em_cartaz/app/modules/details_destaques/detailsDestaques_module.dart';
+import 'package:em_cartaz/app/modules/details_generic/detailsGeneric_module.dart';
 import 'package:em_cartaz/app/modules/home/home_module.dart';
 import 'package:em_cartaz/app/modules/splash/splash_module.dart';
 import 'package:em_cartaz/app/modules/user_identification/userIdentification_module.dart';
@@ -14,5 +15,6 @@ class AppModule extends Module {
     ModuleRoute('/initialConfig/', module: UserIdentificationModule()),
     ModuleRoute('/home/', module: HomeModule()),
     ModuleRoute('/home/details/', module: DetailsDestaquesModule()),
+    ModuleRoute('/home/details/generic', module: DetailsGenericModule()),
   ];
 }
